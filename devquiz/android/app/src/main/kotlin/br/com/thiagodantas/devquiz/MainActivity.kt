41d1d59616ba9c5da4b5c6a320daeb2eeac6204b
Thiago Dantas <thiago_dantas411@hotmail.com>
2021-04-19 14:38:17 -0300

    dia 1
    - App Bar
    -Scorecard

---
+++ devquiz/android/app/src/main/kotlin/br/com/thiagodantas/devquiz/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.thiagodantas.devquiz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ devquiz/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:devquiz/core/core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
